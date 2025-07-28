c86c08d2a4f132622c09d728ae9111d4bc4da2c7
Adamstar124 <139605787+Adamstar124@users.noreply.github.com>
2025-07-29 00:23:29 +0800

    Update exercise_2.56.scm
    debug

--- exercise_2.56.scm
+++ exercise_2.56.scm
@@ -24,7 +24,7 @@
 (define (make-sum a1 a2) 
     (cond ((=number? a1 0) a2)
           ((=number? a2 0) a1)
-          ((and (=number? a1) (=number? n2)) (+ a1 a2))
+          ((and (=number? a1) (=number? a2)) (+ a1 a2))
           (else (list '+ a1 a2))))
       
 (define (make-product p1 p2) 
